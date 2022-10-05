9107469d7250afebb934633aff41e67905489f3d
vijay <vijay.thorat@biddano.com>
2022-10-05 20:10:51 +0530

    Added space between , and sum

--- add.py
+++ add.py
@@ -2,7 +2,7 @@ def addtion(num1,num2):
   a=num1
   b=num2
   sum=a+b
-  print("Sum of the two numbers is :",sum)
+  print("Sum of the two numbers is :", sum)
 
 if __name__ == "__main__":
   addtion(35,47)
